2a1834f503848facb394c8848b7cb68c82b6b928
Himanshuu03 <himanshujangra2403@gmail.com>
2024-04-12 00:51:53 +0530

    vercel

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const port = 8080;
 app.get('/history', (req, res) => {
     res.send(history);
 });
-
 app.get('/geography', (req, res) => {
     res.send(geography);
 });
-
 app.get('/scienceQuestions', (req, res) => {
     res.send(scienceQuestions);
+});
+app.get('/', (req, res) => {
+    res.send('Welcome to the Trivia API!');
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
