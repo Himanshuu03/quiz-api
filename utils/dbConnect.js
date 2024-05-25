247c028a383ff6e4e17ccefc00ef0051c15f18de
Himanshuu03 <himanshujangra2403@gmail.com>
2024-05-25 12:17:51 +0530

    link - db

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async() =>{
     try {    
-        await mongoose.connect("mongodb+srv://himanshu32428:Ih8ZfwFCLLVC94T9@cluster0.n6caph3.mongodb.net/devArcade");
+        await mongoose.connect("mongodb+srv://himanshu32428:Ih8ZfwFCLLVC94T9@cluster0.n6caph3.mongodb.net/devArcade?retryWrites=true&w=majority");
         console.log("Database Connected");
     } catch (error) {
         console.log(`Error in Data Base :-> ${error}`);
